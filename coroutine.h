ae5b27c415939083c9671cdc905861f03f89be5b
João Marques <mrshankly@users.noreply.github.com>
2017-03-19 20:11:29 +0000

    Fix formatting

--- coroutine.h
+++ coroutine.h
@@ -10,6 +10,6 @@ extern co_stack *co_active(void);
 extern void co_resume(co_stack *);
 
 _Static_assert(sizeof(void (*)(void)) == 2, "small code model not enabled");
-_Static_assert(sizeof(void *)         == 2, "small data model not enabled");
+_Static_assert(sizeof(void *) == 2, "small data model not enabled");
 
 #endif
